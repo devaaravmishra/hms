60f46ddf3c55b9f47508344c2b102ecaff5bd1a7
Aarav619 <aaravmishra619@gmail.com>
2021-06-26 22:32:36 +0530

    update remove icon

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,20 +1,26 @@
 import React from "react";
-
-const Navbar= () =>{ 
-    return (
-        <div className={"navbar"}>
-            <div className={"nav-container"}>
-            <i className={"fa fa-hospital-user fa-2x"}></i>&nbsp;&nbsp;
-            <h1 className={"nav-head"}>Hospital</h1>
-            </div>        
-        <ul className={"nav-links"}>
-            <li><a href={"#"}>Home</a></li>
-            <li><a href={"#services"}>Services</a></li>
-            <li><a href={"#"}>Login</a></li>
-            <li><a href={"#"}>Sign Up</a></li>
-        </ul>
+const Navbar = () => {
+  return (
+    <div className={"navbar"}>
+      <div className={"nav-container"}>
+        <h1 className={"nav-head"}>Hospital</h1>
+      </div>
+      <ul className={"nav-links"}>
+        <li>
+          <a href={"#"}>Home</a>
+        </li>
+        <li>
+          <a href={"#services"}>Services</a>
+        </li>
+        <li>
+          <a href={"#"}>Login</a>
+        </li>
+        <li>
+          <a href={"#"}>Sign Up</a>
+        </li>
+      </ul>
     </div>
-    )
-}
+  );
+};
 
 export default Navbar;
