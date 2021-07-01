371e789c177eb3a9c43cc0435c1c54b9c27de1fc
Aarav619 <aaravmishra619@gmail.com>
2021-07-01 23:01:16 +0530

    update remove descreption

--- src/Components/homepage.js
+++ src/Components/homepage.js
@@ -9,75 +9,46 @@ const Homepage = () => {
 
   const onAppointmentBook = () => {
     history.push("/appointments");
+    window.scrollTo(0, 0);
   };
 
   return (
     <React.Fragment>
       <Header />
       <div className={"services"} id={"services"}>
-        <h1 id={"services-head"}>Services We Provide</h1>
+        <h1 id={"services-head"}>Best in Town</h1>
         <div className={"container1"}>
           <div className={"service"}>
             <i className={"fa fa-bed fa-3x"}></i>
             <h2>Emergencies</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              magni enim, impedit quidem iste incidunt nulla optio provident qui
-              ex.
-            </p>
           </div>
           <div className={"service"}>
             <i className={"fa fa-ambulance fa-3x"}></i>
             <h2>Ambulance</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              magni enim, impedit quidem iste incidunt nulla optio provident qui
-              ex.
-            </p>
           </div>
           <div className={"service"}>
             <i className={"fa fa-medkit fa-3x"}></i>
             <h2>Medical Kit</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              magni enim, impedit quidem iste incidunt nulla optio provident qui
-              ex.
-            </p>
           </div>
         </div>
         <div className={"container2"}>
           <div className={"service"}>
             <i className={"fa fa-wheelchair fa-3x"}></i>
             <h2>Wheelchair</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              magni enim, impedit quidem iste incidunt nulla optio provident qui
-              ex.
-            </p>
           </div>
           <div className={"service"}>
             <i className={"fas fa-flask fa-3x"}></i>
             <h2>Laboratory</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              magni enim, impedit quidem iste incidunt nulla optio provident qui
-              ex.
-            </p>
           </div>
           <div className={"service"}>
             <i className={"fas fa-phone fa-3x"}></i>
-            <h2>24x7 Availability</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-              magni enim, impedit quidem iste incidunt nulla optio provident qui
-              ex.
-            </p>
+            <h2>24x7<br/>Availability</h2>
           </div>
         </div>
       </div>
       <div id={"book-appointment"}>
         <h2 id={"book-apt-head"}>
-          Not feeling well? book an appointment with our expertise!
+          Not feeling well? Book an appointment with our Expertise!
         </h2>
         <button id={"book-btn"} onClick={onAppointmentBook}>
           Book
