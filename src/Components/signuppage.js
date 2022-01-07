ce2bc9daf27d6f6da5c15ffe696a04158313af4a
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-07 23:34:31 +0530

    remove <br> tags

--- src/Components/signuppage.js
+++ src/Components/signuppage.js
@@ -7,111 +7,98 @@ import logo from "./imgs/logo.png";
 import "./css/form.css";
 
 const Signup = ({ baseURL, setUserState }) => {
-	const history = useHistory();
-	const [user, setUser] = useState({});
+  const history = useHistory();
+  const [user, setUser] = useState({});
 
-	const onFormSubmit = async (event) => {
-		event.preventDefault();
-		await Axios.post(
-			`https://hmsystem-backend.herokuapp.com/user/signup`,
-			user
-		)
-			.then(({ data }) => {
-				console.info(data);
-				localStorage.setItem("user", JSON.stringify(data));
-				setUserState(data);
-				history.push("/");
-			})
-			.catch((error) => {
-				console.error(error);
-			})
-			.finally(() => {
-				console.info("Login API call finished.");
-			});
-	};
+  const onFormSubmit = async (event) => {
+    event.preventDefault();
+    await Axios.post(`https://hmsystem-backend.herokuapp.com/user/signup`, user)
+      .then(({ data }) => {
+        console.info(data);
+        localStorage.setItem("user", JSON.stringify(data));
+        setUserState(data);
+        history.push("/");
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+      .finally(() => {
+        console.info("Login API call finished.");
+      });
+  };
 
-	return (
-		<React.Fragment>
-			<div id={"super-container"}>
-				<Navbar />
-				<div className={"parent-container"}>
-					<form id={"signin-container"}>
-						<img src={logo} alt={"Health Insurance"} />
-						<br />
-						<br />
-						<div className={"input-container"}>
-							<i className={"fa fa-user icon"}></i>
-							<input
-								className={"logininput"}
-								type={"name"}
-								name={"name"}
-								placeholder={"Name"}
-								required
-								onChange={(e) => {
-									setUser({ ...user, name: e.target.value });
-								}}
-							/>
-						</div>
-						<br />
-						<br />
-						<div className={"input-container"}>
-							<i className={"fa fa-envelope icon"}></i>
-							<input
-								className={"logininput"}
-								type={"email"}
-								name={"email"}
-								placeholder={"Email"}
-								required
-								onChange={(e) => {
-									setUser({ ...user, email: e.target.value });
-								}}
-							/>
-						</div>
-						<br />
-						<br />
-						<div className="input-container">
-							<i className="fa fa-fingerprint icon"></i>
-							<input
-								className={"logininput"}
-								type={"password"}
-								name={"password"}
-								placeholder={"Password"}
-								required
-								onChange={(e) => {
-									setUser({
-										...user,
-										password: e.target.value,
-									});
-								}}
-							/>
-						</div>
-						<br />
-						<br />
-						<div className="input-container">
-							<i className="fa fa-clock icon"></i>
-							<input
-								className={"logininput"}
-								type={"number"}
-								name={"age"}
-								placeholder={"Age"}
-								required
-								onChange={(e) => {
-									setUser({ ...user, age: e.target.value });
-								}}
-							/>
-						</div>
-						<button
-							id={"submit"}
-							type={"submit"}
-							onClick={onFormSubmit}>
-							Create
-						</button>
-					</form>
-				</div>
-			</div>
-			<Footer />
-		</React.Fragment>
-	);
+  return (
+    <React.Fragment>
+      <div id={"super-container"}>
+        <Navbar />
+        <div className={"parent-container"}>
+          <form id={"signin-container"}>
+            <img src={logo} alt={"Health Insurance"} />
+            <br />
+            <div className={"input-container"}>
+              <i className={"fa fa-user icon"}></i>
+              <input
+                className={"logininput"}
+                type={"name"}
+                name={"name"}
+                placeholder={"Name"}
+                required
+                onChange={(e) => {
+                  setUser({ ...user, name: e.target.value });
+                }}
+              />
+            </div>
+            <div className={"input-container"}>
+              <i className={"fa fa-envelope icon"}></i>
+              <input
+                className={"logininput"}
+                type={"email"}
+                name={"email"}
+                placeholder={"Email"}
+                required
+                onChange={(e) => {
+                  setUser({ ...user, email: e.target.value });
+                }}
+              />
+            </div>
+            <div className="input-container">
+              <i className="fa fa-fingerprint icon"></i>
+              <input
+                className={"logininput"}
+                type={"password"}
+                name={"password"}
+                placeholder={"Password"}
+                required
+                onChange={(e) => {
+                  setUser({
+                    ...user,
+                    password: e.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="input-container">
+              <i className="fa fa-clock icon"></i>
+              <input
+                className={"logininput"}
+                type={"number"}
+                name={"age"}
+                placeholder={"Age"}
+                required
+                onChange={(e) => {
+                  setUser({ ...user, age: e.target.value });
+                }}
+              />
+            </div>
+            <button id={"submit"} type={"submit"} onClick={onFormSubmit}>
+              Create
+            </button>
+          </form>
+        </div>
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default Signup;
