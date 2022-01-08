a81f2c25fcaea22a51f0f8cdf3828c4bf8ec72a1
Aarav619 <aaravmishra619@gmail.com>
2022-01-08 22:14:17 +0530

    add: useContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useRef } from "react";
 import {
-  BrowserRouter as Router,
-  Redirect,
-  Route,
-  Switch,
+	BrowserRouter as Router,
+	Redirect,
+	Route,
+	Switch,
 } from "react-router-dom";
 
 import Axios from "axios";
@@ -12,67 +12,81 @@ import Signup from "./Components/signuppage";
 import Homepage from "./Components/homepage";
 import Appointment from "./Components/appointment";
 import AppointmentPage from "./Components/appointmentpage";
+import Navbar from "./Components/navbar";
 
 require("dotenv").config();
 
-const {BASE_URL} = process.env;
+export const Login_Details = React.createContext();
+
+const { BASE_URL } = process.env;
 
 const App = () => {
-  const [user, setUser] = useState({});
-  const isAppoined = useRef(false);
-  const loggedIn = useRef(false);
+	const [user, setUser] = useState({});
+	const isAppoined = useRef(false);
+	const [loggedIn, setloggedIn] = useState(false);
 
-  useEffect(() => {
-    const savedUser = localStorage.getItem("user");
-    if (!loggedIn.current && savedUser) {
-      setUser(JSON.parse(savedUser).user);
-      loggedIn.current = true;
-    }
-  }, [user]);
+	useEffect(() => {
+		const savedUser = localStorage.getItem("user");
+		if (!loggedIn.current && savedUser) {
+			setUser(JSON.parse(savedUser).user);
+			setloggedIn(true);
+		}
+	}, [user]);
 
-  const fetchAppointments = async () => {
-    await Axios.get(`https://hmsystem-backend.herokuapp.com/user/getApt/${user._id}`)
-      .then(({ data }) => {
-        isAppoined.current = true;
-      })
-  };
+	const fetchAppointments = async () => {
+		await Axios.get(
+			`https://hmsystem-backend.herokuapp.com/user/getApt/${user._id}`
+		).then(({ data }) => {
+			isAppoined.current = true;
+		});
+	};
 
-  if(loggedIn.current) {
-  fetchAppointments();
-  }
-
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path={"/"} exact>
-            <Homepage loggedIn = {loggedIn} baseURL={BASE_URL}/>
-          </Route>
-          <Route path={"/login"} >
-            <Login setUserState={setUser} baseURL={BASE_URL}/>
-          </Route>
-          <Route path={"/signup"} >
-            <Signup setUserState={setUser} baseURL={BASE_URL}/>
-          </Route>
-          <Route path={"/appointments"} >
-            <Appointment user={user} loggedIn={loggedIn} baseURL={BASE_URL}/>
-          </Route>
-          <Route path={"/appointmentpage"} >
-            <AppointmentPage user={user} loggedIn={loggedIn} baseURL={BASE_URL}/>
-          </Route>
-          <Route path={"/about-us"}>
-            <Redirect to={"/#about-us"} />
-          </Route>
-          <Route path={"/404"}>
-            <h1>Page not found</h1>
-          </Route>
-          <Route path={"/**"}>
-            <Redirect to={"/404"} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+	if (loggedIn.current) {
+		fetchAppointments();
+	}
+	<Login_Details.Provider value={loggedIn}>
+		<Navbar />
+	</Login_Details.Provider>;
+	return (
+		<Router>
+			<div>
+				<Switch>
+					<Route path={"/"} exact>
+						<Homepage loggedIn={loggedIn} baseURL={BASE_URL} />
+					</Route>
+					<Route path={"/login"}>
+						<Login setUserState={setUser} baseURL={BASE_URL} />
+					</Route>
+					<Route path={"/signup"}>
+						<Signup setUserState={setUser} baseURL={BASE_URL} />
+					</Route>
+					<Route path={"/appointments"}>
+						<Appointment
+							user={user}
+							loggedIn={loggedIn}
+							baseURL={BASE_URL}
+						/>
+					</Route>
+					<Route path={"/appointmentpage"}>
+						<AppointmentPage
+							user={user}
+							loggedIn={loggedIn}
+							baseURL={BASE_URL}
+						/>
+					</Route>
+					<Route path={"/about-us"}>
+						<Redirect to={"/#about-us"} />
+					</Route>
+					<Route path={"/404"}>
+						<h1>Page not found</h1>
+					</Route>
+					<Route path={"/**"}>
+						<Redirect to={"/404"} />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
 };
 
 export default App;
