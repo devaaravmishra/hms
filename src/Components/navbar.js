7496732cfc594d2268d259c1994db7f482e4d3dc
Aarav619 <aaravmishra619@gmail.com>
2022-01-08 18:53:38 +0530

    Format code

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -2,39 +2,43 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
-  const history = useHistory();
-  const refresh = () => {
-    window.location.reload(false);
-  };
-  return (
-    <div className={"navbar"}>
-      <div className={"nav-container"}>
-        <h1
-          onClick={() => {
-            history.push("/");
-            refresh();
-          }}
-          className={"nav-head"}
-        >
-          Hospital
-        </h1>
-      </div>
-      <ul className={"nav-links"}>
-        <li>
-          <a href={"/"}>Home</a>
-        </li>
-        <li>
-        <a href={"/login"}>Login</a>
-        </li>
-        <li>
-        <a href={"/signup"}>Sign Up</a>
-        </li>
-        <li>
-        <a rel="noreferrer" href={"https://github.com/Aarav619/hms-frontend"} target={"_blank"}>About-Us</a>
-        </li>
-      </ul>
-    </div>
-  );
+	const history = useHistory();
+	const refresh = () => {
+		window.location.reload(false);
+	};
+	return (
+		<div className={"navbar"}>
+			<div className={"nav-container"}>
+				<h1
+					onClick={() => {
+						history.push("/");
+						refresh();
+					}}
+					className={"nav-head"}>
+					Hospital
+				</h1>
+			</div>
+			<ul className={"nav-links"}>
+				<li>
+					<a href={"/"}>Home</a>
+				</li>
+				<li>
+					<a href={"/login"}>Login</a>
+				</li>
+				<li>
+					<a href={"/signup"}>Sign Up</a>
+				</li>
+				<li>
+					<a
+						rel="noreferrer"
+						href={"https://github.com/Aarav619/hms-frontend"}
+						target={"_blank"}>
+						About-Us
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Navbar;
