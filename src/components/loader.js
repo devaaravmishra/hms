055175f09aba6fcd344aac4fb43bcb1638bf30b0
Aarav619 <aaravmishra619@gmail.com>
2022-01-12 02:25:06 +0530

    add: loader component

---
+++ src/components/loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../assets/css/loader.css";
+
+const Loader = () => {
+	return (
+		<div className={"parent"}> 
+            <h1 className={"loader-head"}>Fetching your Appointments...</h1>
+		    <div className={"loader"}></div>
+		</div>
+	);
+};
+
+export default Loader;
